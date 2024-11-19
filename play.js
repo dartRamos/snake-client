9d2142245bfe9989bcd7d561010e052ab29756ea
Dartanhan Ramos <alessandroramosd@gmail.com>
2024-11-19 14:46:54 -0500

    Moved connect fuction to client.js and ensured the program still works

--- play.js
+++ play.js
@@ -1,24 +1,5 @@
-// The line `const net = require("net");` imports the 'net' module in Node.js.
-const net = require('net');
-
-// establishes a connectiono with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: 'localhost',
-    port: 50541,
-  })
-
-  // This method interprets incoming data as text in UTF-8 encoding
-  conn.setEncoding('utf8');
-
-  // This code listens for incoming data from the server.
-  conn.on('data', (data) => {
-    console.log(data);
-  });
-
-  return conn;
-};
-
+// Importing conenct function from client
+const connect = require('./client');
 
 
 console.log('Connecting ...');
