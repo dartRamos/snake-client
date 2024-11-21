47e4af3af8febccb7a52dc14b2f634ffe6fb7a21
Dartanhan Ramos <alessandroramosd@gmail.com>
2024-11-21 12:28:51 -0500

    learned how to make the snake move automatically when connecting to the server

--- client.js
+++ client.js
@@ -15,7 +15,28 @@ const connect = function () {
   conn.on('connect', () => {
     console.log('Successfully conneceted to game server!')
     conn.write('Name: DRO'); // Send the player's name to the server
+
+    // setTimeout(() => {
+    //   conn.write('Move: up');
+    //   console.log('Sent: Move up')
+    // }, 0);
+
+    // setTimeout(() => {
+    //   conn.write('Move: right');
+    //   console.log('Sent: Move right')
+    // }, 50);
+
+    // setTimeout(() => {
+    //   conn.write('Move: up');
+    //   console.log('Sent: Move up')
+    // }, 100);
+
+    // setTimeout(() => {
+    //   conn.write('Move: left');
+    //   console.log('Sent: Move left')
+    // }, 150);
   });
+
 
   // This code listens for incoming data from the server.
   conn.on('data', (data) => {
